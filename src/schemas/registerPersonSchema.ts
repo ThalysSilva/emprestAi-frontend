5ec740bc9674008e4ef0cd8de822390229839850
Thalys Farias <thalysfarias11@gmail.com>
2024-08-25 18:38:49 -0300

    feat: create registerPerson schema

---
+++ src/schemas/registerPersonSchema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import moment from 'moment';
+
+export const registerPersonSchema = z.object({
+  name: z.string().min(3, 'Digite um nome').max(120),
+  birthdate: z
+    .string()
+    .min(10, 'Digite uma data de nascimento')
+    .max(10)
+    .refine((value) => {
+      const currentDate = moment();
+      const birthDate = moment(value, 'DD/MM/YYYY');
+      return birthDate.isValid() && birthDate.isBefore(currentDate);
+    }, 'Digite uma data de nascimento válida'),
+  identification: z
+    .string()
+    .min(8, 'Digite uma identificação')
+    .max(14, 'Digite uma identificação válida'),
+});
+
+export type RegisterPersonSchemaData = z.infer<typeof registerPersonSchema>;
