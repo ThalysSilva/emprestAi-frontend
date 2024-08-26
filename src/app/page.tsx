77e1bfb134e4339f306960a729c26c33192f01cc
Thalys Farias <thalysfarias11@gmail.com>
2024-08-25 21:51:31 -0300

    feat: using content home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { BigText2 } from '@/components/Texts';
+import { ContentHome } from './components/ContentHome';
+
 export default async function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col w-full items-center overflow-hidden">
+      <div className="mb-8">
+        <BigText2>Visualizar empréstimos</BigText2>
+      </div>
+      <ContentHome />
     </div>
   );
 }
