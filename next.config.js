b538de2ba4a898816f82ed0f3850485ac3fcd600
Thalys Farias <thalysfarias11@gmail.com>
2024-08-26 13:25:41 -0300

    fix: cors

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)?',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
